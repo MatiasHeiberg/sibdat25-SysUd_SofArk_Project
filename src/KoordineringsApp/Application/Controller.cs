de9a0a8b7c57c0ba58fbe5e3960e322df146af02
Matias Heiberg Nielsen <perm_diners7z@icloud.com>
2026-03-17 11:52:30 +0100

    Refaktor ene controller klasse til den første af mange service klasser

--- src/KoordineringsApp/Application/Controller.cs
+++ src/KoordineringsApp/Application/Controller.cs
@@ -1,21 +1,27 @@
 ﻿using KoordineringsApp.Domæne;
+using KoordineringsApp.FileIO.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace KoordineringsApp.Application
 {
-    internal class Controller
+    public class VehicleService
     {
-
+        private readonly IRepository<Vehicle> _repository;
         public List<Vehicle> Vehicles { get; set; }
 
-        public Controller()
+        public VehicleService(IRepository<Vehicle> repository)
         {
+            _repository = repository;
             Vehicles = new List<Vehicle>();
+            LoadVehicles();
 
+        }
 
-            
+        private void LoadVehicles()
+        {
+
         }
 
     }
---
+++ src/KoordineringsApp/FileIO/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KoordineringsApp.FileIO.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Save();
+        List<T> Load();
+    }
+}
