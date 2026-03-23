f5b22277ba34c0f954c3a8a5a2263f132e8eb7f6
Lasse Agerskov <lasse.agerskov@gmail.com>
2026-03-23 10:49:33 +0100

    Appfacade mm

---
+++ src/KoordineringsApp/Services/AppFacade.cs
@@ -0,0 +1,19 @@
+﻿using KoordineringsApp.Domain;
+using KoordineringsApp.FileIO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KoordineringsApp.Services
+{
+    public class AppFacade
+    {
+        private readonly VehicleService _vehicleService;
+        private readonly BookingService _bookingService;
+
+        public AppFacade(VehicleService vehicleService, BookingService bookingService)
+        {
+
+        }
+    }
+}
